89ce53d2a796593b1c12f80c2331846978c5a193
josep <josep.recio@outlook.com>
2018-05-15 00:17:56 +0200

    Bug fix

--- src/main/java/com/joseprecio/projectefinalcurs/controller/CommandController.java
+++ src/main/java/com/joseprecio/projectefinalcurs/controller/CommandController.java
@@ -66,7 +66,7 @@ public class CommandController {
 		googleAssistantResponse.setFulfillmentText(response.getCommand());
 		
 		//Devolvemos la respuesta
-		return ResponseEntity.ok().body(json.toJson(response));
+		return ResponseEntity.ok().body(json.toJson(googleAssistantResponse));
 	}
 	
 	/**
